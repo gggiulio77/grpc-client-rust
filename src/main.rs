b77371a1d8c71550ce98b04ccf2508a4003647d8
gg <gggiulio77@gmail.com>
2024-04-27 18:36:41 -0300

    feat: add docker and env support

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use grpc_proto::helloworld::{greeter_client::GreeterClient, HelloRequest};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+    dotenv::dotenv().ok();
+
+    let mut client = GreeterClient::connect(std::env::var("SERVER_URL")?).await?;
 
     let request = tonic::Request::new(HelloRequest {
         name: "Coco".into(),
